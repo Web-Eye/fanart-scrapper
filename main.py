cadcbf80a8bbdb8300ef3c0e02c578fc5862cd51
Frank Stochmialek <f.stochmialek@ottcomputer.de>
2023-05-11 15:24:48 +0200

    more testings

--- main.py
+++ main.py
@@ -26,47 +26,25 @@ def do_fanart():
                 print(cover.url)
 
 
-def do_musicbrainz():
-    musicbrainzngs.set_useragent('python-musicplayer-flask', '0.1', '*****@*****.**')
-    # test = musicbrainzngs.search_artists(query='Pearl Jam')
-    # print(test)
+def getMusicBrainzAlbumId(artist_id=None, album=None, format=None, track_count=None, release_date=None):
+    test = musicbrainzngs.search_releases(arid=artist_id, release=album, format=format, date=release_date, tracks=track_count)
 
-    #  '83b9cbe7-9857-49e2-ab8e-b57b01038103'
+    for release in test.get('release-list'):
+        if release.get('title') == album:
+            return release.get('id')
 
-
-    _offset = 0
-    _release_count = 1
-    while _offset + 1 <= _release_count:
-        test = musicbrainzngs.search_releases(query='Vitalogy', offset=_offset)
-        _release_count = test.get('release-count')
-
-        for release in test.get('release-list'):
-            if release.get('title') == 'Vitalogy':
-                #  more decisions
-                #  - release.status == 'Official'
-                #  - release.date == db.strReleaseDate
-                #  - release.medium-list.format == CD
-                #  - release.medium-list.track-count == db.query
-                #  - release.country == 'US/DE'
-
-                _found = False
-                for artist in release.get('artist-credit'):
-                    try:
-                        if artist.get('artist').get('id') == '83b9cbe7-9857-49e2-ab8e-b57b01038103':
-                          _found = True
-                    except:
-                        pass
-
-                if _found:
-                    print(release.get('id'))
-            elif 'Vitalogy' not in release.get('title'):
-                break
-
-        _offset += 25
+def getMusicBrainzArtistId(artist):
+    result = musicbrainzngs.search_artists(artist=artist, limit=100)
+    if result:
+        for a in result['artist-list']:
+            if a['name'] == artist:
+                return a['id']
 
 
-    #  '4816047b-5a40-462a-81e7-2f6eb6687fda'
 
 if __name__ == '__main__':
+    musicbrainzngs.set_useragent('python-musicplayer-flask', '0.1', '*****@*****.**')
     # do_fanart()
-    do_musicbrainz()
+    # print(getMusicBrainzArtistId('Pearl Jam'))
+    # print(getMusicBrainzAlbumId(artist_id='83b9cbe7-9857-49e2-ab8e-b57b01038103', album='Vitalogy', format='CD', track_count=14))
+
